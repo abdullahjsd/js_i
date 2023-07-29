79e9238e62cc034b69ec456a7897e18fa313170a
abdullah <105575210+abdullahjsd@users.noreply.github.com>
2023-07-29 21:42:58 +0300

    değişiklik yapıldı

---
+++ çalışma alanı/script.js
@@ -0,0 +1,27 @@
+const mail= document.querySelector('.email').value;
+const comment= document.querySelector(".txt").value;
+const btn =document.querySelector("btn-danger")
+localStorage.setItem(email,comment)
+localStorage.setItem(email,comment)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
