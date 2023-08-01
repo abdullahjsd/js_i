99a06108cd47d4da5cb0c7b06a6b7f47f04130cc
abdullah <105575210+abdullahjsd@users.noreply.github.com>
2023-08-01 20:14:35 +0300

    balonlar yerleştirildi aktif olarak

--- mayinTarlasi/app.js
+++ mayinTarlasi/app.js
@@ -2,20 +2,39 @@ let parentGround =document.getElementById("parent_ground");
 let denemebutton =document.querySelector(".button");
 let basla        =document.getElementById("basla")
 let boxes        =document.querySelectorAll(".box")
-
  
 
+let kare = 63;
+let bomba=5;
+ 
 
-
-for(i=0;i<100;i++){
+let rst =Math.floor(Math.random() * kare);
+for(i=0;i<kare;i++){
      let boxes = document.createElement("div");
-     boxes.classList.add("box")
-     parentGround.appendChild(boxes);
-    
- 
-     boxes.addEventListener("click",function(){
-          
-     })
+          boxes.classList.add("box")
+          parentGround.appendChild(boxes);
+
+     let balonimg     =document.createElement("img")
+          balonimg.style.width="65px"
+          balonimg.classList.add("sekil")
+          balonimg.src=("img/balon.jpg")
+          boxes.appendChild(balonimg);
+ /******************************* */
+          boxes.addEventListener("click",function(){
+               for(i=0;i<kare;i++){
+                    console.log(rst);
+               }
+                    
+                         
+               
+               
+               // if(rst==patla){
+               //           console.log("bonba patadı");
+               //      }
+               //      else{
+               //           console.log("patlamaı");
+               //      }
+          })
    
 }
 
