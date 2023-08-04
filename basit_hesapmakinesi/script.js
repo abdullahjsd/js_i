1585a703a8bc02119b90a1d67b9e3f9c41aca860
abdullah <105575210+abdullahjsd@users.noreply.github.com>
2023-08-04 23:39:33 +0300

    hesap makinesi

--- basit_hesapmakinesi/script.js
+++ basit_hesapmakinesi/script.js
@@ -1,3 +1,17 @@
-const buttons=document.querySelectorAll("button")
+const input =document.querySelector("input")
+const buttons   =document.querySelector(".buttons")
 
-console.log(buttons);
+
+ 
+let inputValue="0"
+
+myfunction()
+function myfunction(){
+    input.value=inputValue
+}
+
+buttons.addEventListener("click",function(e){
+    const element=e.target
+    if(!element.matches("button"))return;
+
+})
