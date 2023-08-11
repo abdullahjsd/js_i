f48de8c50d23c3d7e45b5776b4e45da107b7c1a8
abdullah <105575210+abdullahjsd@users.noreply.github.com>
2023-08-11 22:47:07 +0300

    dom dan veriler alındaı

--- basit_hesapmakinesi/script.js
+++ basit_hesapmakinesi/script.js
@@ -1,17 +1,37 @@
-const input =document.querySelector("input")
-const buttons   =document.querySelector(".buttons")
-
-
+const input =document.querySelector(".Cinput")
+let buttons   =document.querySelector(".buttons")
  
+
 let inputValue="0"
 
-myfunction()
-function myfunction(){
+
+update()
+ function update(){
     input.value=inputValue
 }
 
 buttons.addEventListener("click",function(e){
-    const element=e.target
-    if(!element.matches("button"))return;
+    const element = e.target
+    if(!element.matches("button")) return;
 
+    if(element.classList.contains("operator")){
+        console.log("operatör");
+        return
+    }
+    if(element.classList.contains("decimal")){
+        console.log("decimal");
+        return
+    }
+    if(element.classList.contains("clear")){
+        console.log("clear");
+        return
+    }
+ 
+    inputNumber(element.value)
+   update()
 })
+
+function inputNumber(num){
+    inputValue=num
+}
+
