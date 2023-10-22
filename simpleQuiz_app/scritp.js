d5490a130e5186e23d0e3030f1429864b0dac50a
abdullah <105575210+abdullahjsd@users.noreply.github.com>
2023-10-22 20:40:35 +0300

    question app and

--- simpleQuiz_app/scritp.js
+++ simpleQuiz_app/scritp.js
@@ -56,15 +56,16 @@ let currentQuestionIndex = 0;
 let score =0;
 
 function startQuiz(){
-    currentQuestionIndex = 0
+    currentQuestionIndex = 0;
     score=0
     nextBtn.innerHTML = "Next"
     showQuestion()
 }
 
 function showQuestion(){
+    resetState()
     let currentQuestion = questions[currentQuestionIndex]
-    let questionNo = currentQuestionIndex
+    let questionNo = currentQuestionIndex + 1 
     questionsElement.innerHTML = questionNo +". " + currentQuestion.question;
 
     currentQuestion.answer.forEach(answer =>{
@@ -72,9 +73,65 @@ function showQuestion(){
         button.innerHTML = answer.text
         button.classList.add("btn")
         answerButtons.appendChild(button)
+        if(answer.correct){
+            button.dataset.correct= answer.correct
+        }
+        button.addEventListener("click", sectEnswer)
     })
 
 
 
 
 }
+
+function resetState(){
+    nextBtn.style.display="none"
+    while(answerButtons.firstChild){
+        answerButtons.removeChild(answerButtons.firstChild)
+    }
+}
+
+function sectEnswer(e){
+    const selectedBtn= e.target
+    const isCorrect = selectedBtn.dataset.correct === "true"
+    if(isCorrect){
+        selectedBtn.classList.add("correct")
+    }else{
+        selectedBtn.classList.add("incorrect")
+    }
+    Array.from(answerButtons.children).forEach(button=>{
+        if(button.dataset.correct==="true"){
+            button.classList.add("correct")
+        }
+        button.Disabled = "true"
+      });
+      nextBtn.style.display ="block"
+}
+
+function showScore(){
+    resetState()
+    questionsElement.innerHTML=`you score ${score} out of ${questions.length}!`
+    nextBtn.innerHTML = "Play Again"
+    nextBtn.style.display = "block"
+}
+
+function handleNextBotton(){
+    currentQuestionIndex++
+    if(currentQuestionIndex<questions.length){
+        showQuestion()
+    }else{
+        showScore()
+    }
+}
+
+
+
+nextBtn.addEventListener("click",()=>{
+    if(currentQuestionIndex<questions.length){
+        handleNextBotton()
+    }else{
+        startQuiz()
+    }
+})
+
+startQuiz()
