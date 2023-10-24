18538c01f9ef245b004feec7e68a77caa75c150f
abdullah <105575210+abdullahjsd@users.noreply.github.com>
2023-10-24 23:41:06 +0300

    js start

---
+++ sifre Üret/sicript.js
@@ -0,0 +1,70 @@
+const passwordBox = document.getElementById("password")
+const lenght = 12;
+
+const upperCase ="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const loweCase  ="abcdefghijklmnopqrstuvwxyz"
+const number = "0123456789"
+const symbol = "@#$%/-+()*|=]}[{<>"
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
