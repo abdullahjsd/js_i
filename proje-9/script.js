fff670396980f1ddc50b2c1705019a3ada31a75a
abdullah <105575210+abdullahjsd@users.noreply.github.com>
2023-11-01 20:22:58 +0300

    hangman

---
+++ proje-9/script.js
@@ -0,0 +1,129 @@
+const letterContainer = document.getElementById("letter-container")
+const optoinsContainer = document.getElementById("optoins-container")
+const userInputSection = document.getElementById("user-input-section")
+const canvas = document.getElementById("canvas")
+const newGameContainer = document.getElementById("new-game-container")
+const resultText = document.getElementById("result-text")
+const newGameButton = document.getElementById("new-game-button")
+
+
+let options = {
+    fruits:[
+        "Apple",
+        "Blueberry",
+        "Mandarin",
+        "Pineapple",
+        "Pomegranete",
+        "Watermelon",
+    ],
+    animals:[
+        "Hedgehop",
+        "Rhinoceros",
+        "Squirrel",
+        "Panther",
+        "Walrus",
+        "Zebra",
+    ],
+    countries:[
+        "India",
+        "Hungary",
+        "Kyrgyzstan",
+        "Switzerland",
+        "Zimbabawe",
+        "Dominica",
+    ]
+}
+
+//count
+let winCount = 0;
+let count = 0;
+
+let chosenWord = "";
+
+// display option buttons
+const displayOptions = () =>{
+    optoinsContainer.innerHTML += `<`
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
