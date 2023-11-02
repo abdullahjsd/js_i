6120b5f08d9d6da9ccbcb77a9054b6637d80d995
abdullah <105575210+abdullahjsd@users.noreply.github.com>
2023-11-02 16:50:49 +0300

    26 dk

--- proje-9/script.js
+++ proje-9/script.js
@@ -42,10 +42,63 @@ let chosenWord = "";
 
 // display option buttons
 const displayOptions = () =>{
-    optoinsContainer.innerHTML += `<`
+    optoinsContainer.innerHTML += `<h3>Please Select An Option</h3>`
+    let buttonCon = document.createElement("div");
+    for(let value in options){
+        buttonCon.innerHTML += `<button class="options" onclick="generatorWord=('${value}')">${value}</button>`
+        
+    }
+    optoinsContainer.appendChild(buttonCon)
 }
 
+//word generator
+let generatorWord = (optionValue) =>{
+    let optionsButtons = document.querySelectorAll("options");
 
+    //if opton value matshes teh button innerText the highlight teh buton
+    optionsButtons.forEach((button) => {
+        if(button.innerText.toLowerCase() === optionValue ){
+            button.classList.add("active");
+        }
+        button.disabled = true;
+    
+    })
+    //initially hid letters
+    letterContainer.classList.remove("hide");
+    userInputSection.innerText = "";
+
+    let optionArray = options[optionValue];
+    //choose random word
+    chosenWord = optionArray[Math.floor(Math.random () * optionArray.length)];
+    chosenWord = chosenWord.toLocaleUpperCase();
+    console.log(chosenWord)
+
+    //replace every letter..
+    let dislayItem = chosenWord.replace(/./g,'<span class="dashes">_</span>');
+
+    //display each element...
+    userInputSection.innerHTML = dislayItem;
+};
+
+
+//Inital function (called whe page load.....)
+const initializer = () =>{
+    winCount = 0;
+    count = 0 ;
+
+    //for creatin letter buttons
+    for (let i =65;i<91 ; i++){
+        let button = document.createElement("button");
+        button.classList.add("letters")
+        //Number to a-z
+        button.innerText = String.fromCharCode(i);
+        letterContainer.append(button)
+    }
+    displayOptions()
+}
+// New game
+newGameButton.addEventListener("click",initializer);
+window.onload = initializer;
 
 
 
